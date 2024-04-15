ae2ecebac50cd69884f1313f877dfccc3e084f86
a926065 <pablo.navarro.external@eviden.com>
2024-04-15 10:54:40 +0200

    root commit

---
+++ src/main/java/com/eviden/v0/ClubNauticoJpaApplication.java
@@ -0,0 +1,13 @@
+package com.eviden.v0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubNauticoJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubNauticoJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eviden/v0/ClubNauticoJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eviden.v0;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubNauticoJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
